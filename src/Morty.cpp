12f03debe5e3166209280dde8388eb421b3c03e8
UA-joealexliang <70230362+UA-joealexliang@users.noreply.github.com>
2020-09-23 17:57:44 -0700

    Update Morty.cpp

--- src/Morty.cpp
+++ src/Morty.cpp
@@ -16,32 +16,39 @@
 
 namespace C137 {
 
-	void OutputC137(int i) {
-		if (i % 15 == 0) {
-			std::cout << i << ": Aww Geez Man\n";
-		}
-		else if (i % 3 == 0) {
-			std::cout << i << ": Aww\n";
-		}
-		else if (i % 5 == 0) {
-			std::cout << i << ": Geez\n";
-		}
-		else {
-			std::cout << i << ": \n";
-		}
-	}
-
 	void PrintAwwGeezMan(int start, int stop) {
 		std::cout << "Morty C137 says:\n";
 		for (int i = start; i <= stop; i++) {
-			C137::OutputC137(i);
+			if (i % 15 == 0) {
+				std::cout << i << ": Aww Geez Man\n";
+			}
+			else if (i % 3 == 0) {
+				std::cout << i << ": Aww\n";
+			}
+			else if (i % 5 == 0) {
+				std::cout << i << ": Geez\n";
+			}
+			else {
+				std::cout << i << ": \n";
+			}
 		}
 	}
 
 	void PrintAwwGeezMan(int start, int stop, int step) {
 		std::cout << "Morty C137 says:\n";
 		for (int i = start; i <= stop; i = i + step) {
-			C137::OutputC137(i);
+			if (i % 15 == 0) {
+				std::cout << i << ": Aww Geez Man\n";
+			}
+			else if (i % 3 == 0) {
+				std::cout << i << ": Aww\n";
+			}
+			else if (i % 5 == 0) {
+				std::cout << i << ": Geez\n";
+			}
+			else {
+				std::cout << i << ": \n";
+			}
 		}
 	}
 
@@ -53,32 +60,39 @@ namespace C137 {
 
 namespace Z286 {
 
-	void OutputZ286(int i) {
-		if (i % 15 == 0) {
-			std::cout << i << ": naM zeeG wwA\n";
-		}
-		else if (i % 3 == 0) {
-			std::cout << i << ": wwA\n";
-		}
-		else if (i % 5 == 0) {
-			std::cout << i << ": zeeG\n";
-		}
-		else {
-			std::cout << i << ": \n";
-		}
-	}
-
 	void PrintAwwGeezMan(int start, int stop) {
 		std::cout << "Morty Z286 says:\n";
 		for (int i = start; i <= stop; i++) {
-			Z286::OutputZ286(i);
+			if (i % 15 == 0) {
+				std::cout << i << ": naM zeeG wwA\n";
+			}
+			else if (i % 3 == 0) {
+				std::cout << i << ": wwA\n";
+			}
+			else if (i % 5 == 0) {
+				std::cout << i << ": zeeG\n";
+			}
+			else {
+				std::cout << i << ": \n";
+			}
 		}
 	}
 
 	void PrintAwwGeezMan(int start, int stop, int step) {
 		std::cout << "Morty Z286 says:\n";
 		for (int i = start; i <= stop; i = i + step) {
-			Z286::OutputZ286(i);
+			if (i % 15 == 0) {
+				std::cout << i << ": naM zeeG wwA\n";
+			}
+			else if (i % 3 == 0) {
+				std::cout << i << ": wwA\n";
+			}
+			else if (i % 5 == 0) {
+				std::cout << i << ": zeeG\n";
+			}
+			else {
+				std::cout << i << ": \n";
+			}
 		}
 	}
 
